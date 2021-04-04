96a2d5a1a79a99cdf8ac5634d8cbd7aaac84f21e
Jolene <jolene0443@gmail.com>
2021-04-04 14:11:17 +0800

    feat: Set up layout of Consumer page

---
+++ vapp/src/views/consumer/ConsumerProfile.vue
@@ -0,0 +1,134 @@
+<template>
+  <div>
+    <!-- Sidenav -->
+    <div class="row">
+      <div class="profile left">
+          <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="" width="130" height="130">
+          <div class="name">{{ this.name }}</div>
+          <div class="userType">Consumer</div>
+      </div>
+
+      <!-- Main -->
+      <div class="main right">
+        <h2>Personal Details</h2><br>
+        <div class="card">
+          <div class="card-body">
+            <table>
+              <tbody>
+              <tr>
+                <td>Name</td>
+                <td>:</td>
+                <td>{{ this.name }}</td>
+              </tr>
+              <tr>
+                <td>ID</td>
+                <td>:</td>
+                <td>{{ this.id }}</td>
+              </tr>
+              <tr>
+                <td>Token Balance</td>
+                <td>:</td>
+                <td>{{ this.balance }}</td>
+              </tr>
+              <tr>
+                <td>Emissions</td>
+                <td>:</td>
+                <td>{{ this.emissions }}</td>
+              </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {mapGetters} from "vuex";
+
+export default {
+  name: 'ConsumerProfile',
+  computed: {
+    ...mapGetters('drizzle', ['isDrizzleInitialized', 'drizzleInstance']),
+    ...mapGetters("contracts", ["getContractData"]),
+  },
+  data() {
+    return {
+      userType: '',
+      id: '1',
+      name: 'jojo',
+      balance: 100,
+      emissions: 130
+    };
+  },
+  methods: {
+  },
+};
+
+</script>
+
+<style scoped>
+/* Sidenav */
+.profile {
+  margin-bottom: 20px;
+  margin-top: -12px;
+  text-align: center;
+}
+
+.profile img {
+  border-radius: 50%;
+  box-shadow: 0px 0px 5px 1px grey;
+}
+
+.name {
+  font-size: 20px;
+  font-weight: bold;
+  padding-top: 20px;
+}
+
+.userType {
+  font-size: 16px;
+  font-weight: bold;
+  padding-top: 10px;
+}
+
+/* Main */
+.main .card {
+  background-color: #fff;
+  border-radius: 18px;
+  box-shadow: 1px 1px 8px 0 grey;
+  height: auto;
+  margin-bottom: 20px;
+  padding: 20px 0 20px 50px;
+}
+
+.main .card table {
+  border: none;
+  font-size: 18px;
+  height: 270px;
+  width: 40%;
+}
+
+.row {
+  display: flex;
+}
+
+.left {
+  background-color: #fff;
+  color: #333;
+  text-align: center;
+  padding: 30px;
+  padding-top: 50px;
+  flex: 30%;
+  margin-top: 30px;
+}
+
+.right {
+  padding: 30px;
+  flex: 65%;
+  text-align: left;
+  margin-right: 40px;
+}
+
+</style>
