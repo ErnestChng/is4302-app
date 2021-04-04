7561db19d17dc353705c8faa9f0bc12fff4a873e
ernestchng <ernestchngyongcheng97@gmail.com>
2021-04-05 02:50:37 +0800

    fix: Updated CarbonCredit solidity file

--- contracts/CarbonCredit.sol
+++ contracts/CarbonCredit.sol
@@ -4,6 +4,7 @@ import "./token/ERC20/ERC20.sol";
 
 contract CarbonCredit is ERC20 {
 
+    // ==== VARIABLES ==== //
     address public contractOwner;
 
     struct Generator {
@@ -16,12 +17,12 @@ contract CarbonCredit is ERC20 {
     struct Consumer {
         uint id;
         uint tokenBalance;
-        uint emissions; //current emissions so far
+        uint emissions;
         string consumerName;
         address consumerAddress;
     }
 
-    mapping(uint => Generator) public allGenerators;
+    mapping(uint => Generator) allGenerators;
     mapping(uint => Consumer) allConsumers;
     mapping(address => bool) allValidators;
 
@@ -33,14 +34,12 @@ contract CarbonCredit is ERC20 {
     uint[] violatorsList;
     uint numExceeded;
 
-    // constructor() public ERC20("Carbon Token", "c") {
-    //     contractOwner = msg.sender;
-    // }
-
+    // ==== CONSTRUCTOR ==== //
     constructor() public ERC20() {
         contractOwner = msg.sender;
     }
 
+    // ==== MODIFIERS ==== //
     modifier isValidator() {
         require(allValidators[msg.sender] == true);
         _;
@@ -51,13 +50,7 @@ contract CarbonCredit is ERC20 {
         _;
     }
 
-    // TEST VARIABLES AND FUNCTIONS - TO REMOVE
-    uint public carbondata = 987654321;
-
-    function changecarbondata() public {
-        carbondata = 12345;
-    }
-
+    // ==== GETTERS ==== //
     function getGenerator(uint generatorId) public view returns (bool) {
         return generatorExists[generatorId];
     }
@@ -66,7 +59,11 @@ contract CarbonCredit is ERC20 {
         return generatorList;
     }
 
-    function getViolators() public view returns (uint[] memory){ // for consumers
+    function getConsumerList() public view returns (uint[] memory) {
+        return consumerList;
+    }
+
+    function getViolators() public view returns (uint[] memory){// for consumers
         return violatorsList;
     }
 
@@ -74,89 +71,6 @@ contract CarbonCredit is ERC20 {
         return contractOwner;
     }
 
-    //Create new generator and store in mapping, assign values (balance = 0)
-    function createGenerator(uint generatorId, string memory name, address thisAdd) public isRegulator {
-        require(generatorExists[generatorId] == false);
-
-        Generator memory newGenerator = Generator(
-            generatorId,
-            0,
-            name,
-            thisAdd
-        );
-
-        allGenerators[generatorId] = newGenerator;
-        generatorExists[generatorId] = true;
-
-        generatorList.push(generatorId);
-
-    }
-    //Create new consumer and store in mapping, assign values (balance = limit)
-    function createConsumer(uint consumerId, string memory name, address thisAdd) public isRegulator {
-        require(consumerExists[consumerId] == false);
-
-        Consumer memory newConsumer = Consumer(
-            consumerId,
-            100,
-            0,
-            name,
-            thisAdd
-        );
-
-        allConsumers[consumerId] = newConsumer;
-        consumerExists[consumerId] = true;
-        consumerList.push(consumerId);
-    }
-
-
-    //Award tokens to generators when carbon saving projects are approved
-    function allocateCredit(uint generatorId, uint credit) public isValidator {
-        require(generatorExists[generatorId] == true);
-        require(credit > 0);
-
-        //_mint(contractOwner, allGenerators[generatorId].generatorAddress, credit, "receive", "fromRegulator");
-        _mint(allGenerators[generatorId].generatorAddress, credit);
-        allGenerators[generatorId].tokenBalance += credit;
-
-    }
-
-    //Validators input a emissions amt (frontend), add it to consumer's emissions value
-    function reportEmission(uint consumerId, uint emission) public isValidator{
-        require(emission > 0);
-        require(consumerExists[consumerId] == true);
-
-        allConsumers[consumerId].emissions = emission;
-
-        // Add consumerId to violatorsList if emissions exceed tokenBalance.
-        if (emission > allConsumers[consumerId].tokenBalance) {
-            violatorsList.push(consumerId);
-        }
-    }
-
-    // Cant put int, must put uint, if not it screws up the entire contract
-    function updateConsumerBalance(uint consumerId, uint delta, bool subtract) public {
-        require(consumerExists[consumerId] == true);
-
-        if (subtract) {
-            allConsumers[consumerId].tokenBalance -= delta;
-        } else {
-            allConsumers[consumerId].tokenBalance += delta;
-        }
-
-    }
-
-    // Cant put int, must put uint, if not it screws up the entire contract
-    function updateGeneratorBalance(uint generatorId, uint delta, bool subtract) public {
-        require(generatorExists[generatorId] == true);
-
-        if (subtract) {
-            allGenerators[generatorId].tokenBalance -= delta;
-        } else {
-            allGenerators[generatorId].tokenBalance += delta;
-        }
-    }
-
-    // GETTERS
     function getConsumerCredits(uint consumerId) public view returns (uint credit) {
         require(consumerExists[consumerId] == true);
         return (allConsumers[consumerId].tokenBalance);
@@ -179,17 +93,94 @@ contract CarbonCredit is ERC20 {
         require(consumerExists[consumerId] == true);
         return (allConsumers[consumerId].consumerName);
     }
+
     function getConsumerEmission(uint consumerId) public view returns (uint emission) {
         require(consumerExists[consumerId] == true);
         return (allConsumers[consumerId].emissions);
     }
 
-    // //For each consumer in mapping, check if emissions > tokenBalance. If exceeded, adds to result list.
+    // ==== FUNCTIONS ==== //
+    function createGenerator(uint generatorId, string memory name, address thisAdd) public isRegulator {
+        require(generatorExists[generatorId] == false);
+
+        Generator memory newGenerator = Generator(
+            generatorId,
+            0,
+            name,
+            thisAdd
+        );
+
+        allGenerators[generatorId] = newGenerator;
+        generatorExists[generatorId] = true;
+
+        generatorList.push(generatorId);
+    }
+
+    function createConsumer(uint consumerId, string memory name, address thisAdd) public isRegulator {
+        require(consumerExists[consumerId] == false);
+
+        Consumer memory newConsumer = Consumer(
+            consumerId,
+            100,
+            0,
+            name,
+            thisAdd
+        );
+
+        allConsumers[consumerId] = newConsumer;
+        consumerExists[consumerId] = true;
+        consumerList.push(consumerId);
+    }
+
+    function allocateCredit(uint generatorId, uint credit) public
+        //    isValidator
+    {
+        require(generatorExists[generatorId] == true);
+        require(credit > 0);
+
+        _mint(allGenerators[generatorId].generatorAddress, credit);
+        allGenerators[generatorId].tokenBalance += credit;
+    }
+
+    function reportEmissions(uint consumerId, uint emission) public
+        //    isValidator
+    {
+        require(emission > 0);
+        require(consumerExists[consumerId] == true);
+
+        allConsumers[consumerId].emissions = emission;
+
+        // Add consumerId to violatorsList if emissions exceed tokenBalance.
+        if (emission > allConsumers[consumerId].tokenBalance) {
+            violatorsList.push(consumerId);
+        }
+    }
+
+    function updateConsumerBalance(uint consumerId, uint delta, bool subtract) public {
+        require(consumerExists[consumerId] == true);
+
+        if (subtract) {
+            allConsumers[consumerId].tokenBalance -= delta;
+        } else {
+            allConsumers[consumerId].tokenBalance += delta;
+        }
+    }
+
+    function updateGeneratorBalance(uint generatorId, uint delta, bool subtract) public {
+        require(generatorExists[generatorId] == true);
+
+        if (subtract) {
+            allGenerators[generatorId].tokenBalance -= delta;
+        } else {
+            allGenerators[generatorId].tokenBalance += delta;
+        }
+    }
+
     function checkEmission() isRegulator public view returns (uint[] memory, uint[] memory, uint[] memory, uint[] memory) {
-        //Returns 4 arrays: ID, balance, emissions and exceededBy. Same index corresponds to same company.
+        // Returns 4 arrays: ID, balance, emissions and exceededBy. Same index corresponds to same company.
         uint resCount;
 
-        for (uint i=0; i<consumerList.length; i++) {
+        for (uint i = 0; i < consumerList.length; i++) {
             uint thisID = consumerList[i];
             uint thisBalance = allConsumers[thisID].tokenBalance;
             uint thisEmission = allConsumers[thisID].emissions;
@@ -202,10 +193,10 @@ contract CarbonCredit is ERC20 {
         uint[] memory resID = new uint[](resCount);
         uint[] memory resBalance = new uint[](resCount);
         uint[] memory resEmission = new uint[](resCount);
-        uint[] memory resExceededBy= new uint[](resCount);
-        uint j;
+        uint[] memory resExceededBy = new uint[](resCount);
+        uint j = 0;
 
-        for (uint i=0; i<consumerList.length; i++) {
+        for (uint i = 0; i < consumerList.length; i++) {
             uint thisID = consumerList[i];
             uint thisBalance = allConsumers[thisID].tokenBalance;
             uint thisEmission = allConsumers[thisID].emissions;
@@ -222,6 +213,5 @@ contract CarbonCredit is ERC20 {
 
         return (resID, resBalance, resEmission, resExceededBy);
     }
-
 
 }
