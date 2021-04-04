3757d545f51c45d9fe55938f124871e699e50339
ernestchng <ernestchngyongcheng97@gmail.com>
2021-04-05 02:53:48 +0800

    feat: Created BackButton component

---
+++ vapp/src/components/BackButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <button onclick="history.go(-1)"><span>Go Back</span></button>
+</template>
+
+<script></script>
+
+<style scoped>
+button {
+  margin-left: auto;
+  margin-right: 40px;
+
+  text-align: center;
+  padding: 15px;
+  transition: all 0.5s;
+  cursor: pointer;
+}
+
+button span {
+  cursor: pointer;
+  display: inline-block;
+  position: relative;
+  transition: 0.5s;
+}
+
+button span:after {
+  content: '\00bb';
+  position: absolute;
+  opacity: 0;
+  top: 0;
+  right: -20px;
+  transition: 0.5s;
+}
+
+button:hover span {
+  padding-right: 25px;
+}
+
+button:hover span:after {
+  opacity: 1;
+  right: 0;
+}
+</style>
