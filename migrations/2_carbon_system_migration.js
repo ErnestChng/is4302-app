481a1dfaa83ae6d50d55cbd7e87cf3bbf7376ed6
ernestchng <ernestchngyongcheng97@gmail.com>
2021-04-08 01:10:52 +0800

    feat: Updated migration script

--- migrations/2_carbon_system_migration.js
+++ migrations/2_carbon_system_migration.js
@@ -1,15 +1,39 @@
-const CarbonCredit = artifacts.require("CarbonCredit");
+// const CarbonCredit = artifacts.require("CarbonCredit");
+// const MarketPlace = artifacts.require("MarketPlace");
+//
+// module.exports = function (deployer, network, accounts) {
+//     const platform = accounts[0];
+//     let collectibleInstance;
+//
+//     return deployer
+//         .then(() => {
+//             return deployer.deploy(CarbonCredit, {from: platform});
+//         }).then((_inst) => {
+//             collectibleInstance = _inst;
+//             return deployer.deploy(MarketPlace, collectibleInstance.address, {from: platform});
+//         });
+// };
+
+const ERC20 = artifacts.require("ERC20");
+const carbonCredit = artifacts.require("CarbonCredit");
 const MarketPlace = artifacts.require("MarketPlace");
 
 module.exports = function (deployer, network, accounts) {
-    const platform = accounts[0];
-    let collectibleInstance;
-
-    return deployer
-        .then(() => {
-            return deployer.deploy(CarbonCredit, {from: platform});
-        }).then((_inst) => {
-            collectibleInstance = _inst;
-            return deployer.deploy(MarketPlace, collectibleInstance.address, {from: platform});
+    deployer.then(() => {
+        return deployer.deploy(ERC20, {
+            from: accounts[0]
         });
+    }).then(ERC20Instance => {
+        console.log("ERC20 contract at address = " + ERC20Instance.address);
+        return deployer.deploy(carbonCredit, {
+            from: accounts[0]
+        });
+    }).then(carbonCreditInstance => {
+        console.log("Carbon Credit contract at address = " + carbonCreditInstance.address);
+        return deployer.deploy(MarketPlace, carbonCreditInstance.address, {
+            from: accounts[0]
+        });
+    }).then(MarketPlaceInstance => {
+        console.log("MarketPlace contract at address = " + MarketPlaceInstance.address);
+    });
 };
