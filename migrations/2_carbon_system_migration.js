3ac974fd859595c96da4743871c63899ad6b1e18
ernestchng <ernestchngyongcheng97@gmail.com>
2021-04-08 17:24:32 +0800

    edit: Updated migration scripts to set validator to activeAccount

--- migrations/2_carbon_system_migration.js
+++ migrations/2_carbon_system_migration.js
@@ -1,37 +1,27 @@
-// const CarbonCredit = artifacts.require("CarbonCredit");
-// const MarketPlace = artifacts.require("MarketPlace");
-//
-// module.exports = function (deployer, network, accounts) {
-//     const platform = accounts[0];
-//     let collectibleInstance;
-//
-//     return deployer
-//         .then(() => {
-//             return deployer.deploy(CarbonCredit, {from: platform});
-//         }).then((_inst) => {
-//             collectibleInstance = _inst;
-//             return deployer.deploy(MarketPlace, collectibleInstance.address, {from: platform});
-//         });
-// };
-
 const ERC20 = artifacts.require("ERC20");
 const carbonCredit = artifacts.require("CarbonCredit");
 const MarketPlace = artifacts.require("MarketPlace");
 
+
 module.exports = function (deployer, network, accounts) {
+    // for demonstration purpose: activeAccount will represent a Regulator and Validator
+    const activeAccount = accounts[0];
+
     deployer.then(() => {
         return deployer.deploy(ERC20, {
-            from: accounts[0]
+            from: activeAccount
         });
     }).then(ERC20Instance => {
         console.log("ERC20 contract at address = " + ERC20Instance.address);
         return deployer.deploy(carbonCredit, {
-            from: accounts[0]
+            from: activeAccount // role of regulator is assigned to contractOwner by default
         });
     }).then(carbonCreditInstance => {
+        // assign role of validator to activeAccount
+        carbonCreditInstance.createValidator(activeAccount);
         console.log("Carbon Credit contract at address = " + carbonCreditInstance.address);
         return deployer.deploy(MarketPlace, carbonCreditInstance.address, {
-            from: accounts[0]
+            from: activeAccount
         });
     }).then(MarketPlaceInstance => {
         console.log("MarketPlace contract at address = " + MarketPlaceInstance.address);
