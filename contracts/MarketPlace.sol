db37f29764431d69ce3d86a44ea135487af11762
Jolene <jolene0443@gmail.com>
2021-04-12 11:47:23 +0800

    fix: Update MarketPlace contract

--- contracts/MarketPlace.sol
+++ contracts/MarketPlace.sol
@@ -49,6 +49,18 @@ contract MarketPlace {
         return numListings;
     }
 
+    function getMinIdx(uint[] memory price) public view returns (uint) {
+        uint minValue = 1000000;
+        uint minIdx; 
+        for (uint i = 0; i < numListings; i++) {
+            if(price[i] > 0 && price[i] < minValue){
+                minValue = prices[i];
+                minIdx = i;
+            }
+        }
+        return minIdx;
+    }
+
     // ==== FUNCTIONS ==== //
     // this gets added to the sorted list of prices. We will prevent people from listing
     function listCredit(uint firmId, uint price, uint quantity) public {
@@ -71,14 +83,13 @@ contract MarketPlace {
         listing memory newListing = listing(firmId, quantity, true);
         creditsForSale[price] = newListing;
 
-        // add the same thing to the list of prices, then sort
+        // add the same thing to the list of prices, return unsorted list
         prices.push(price);
-        qty.push(quantity); // TODO: check with shervin
+        qty.push(quantity); 
         numListings += 1;
-        insertionSort(prices, numListings);
 
         //transfer tokens from the seller to the marketplace
-        carbonCredit.transferFrom(msg.sender, address(this), quantity);
+        carbonCredit.transferFrom(msg.sender, marketplaceOwner, quantity);
         if (consumer) {
             carbonCredit.updateConsumerBalance(firmId, quantity, true);
         } else {
@@ -86,72 +97,58 @@ contract MarketPlace {
         }
     }
 
-    function buyCredit(uint firmId, uint quantity) public onlyConsumer(firmId) returns (uint numFilled, uint avgPriceFilled) {
-        // looks at creditsForSale and picks the lowest price.
-        // this process is repeated until the entire buy order is satisfied
+    function buyCredit(uint buyer, uint quantity) public onlyConsumer(buyer) returns (uint numFilled, uint avgPriceFilled) {
         assert(quantity > 0);
-        assert(carbonCredit.isConsumer(firmId));
 
-        uint filledSoFar = 0;
+        uint filled = 0; 
         uint totalPaid = 0;
 
-        // while loop runs either until we fill the quantity, or we run out of listings
-        while (filledSoFar < quantity) {
-            if (prices[0] == 0 || numListings == 0) {// this is the case where there are no more listings
+        while(filled < quantity){
+            //if there are no more listings, break out of loop
+            if(numListings == 0){
+                
+                carbonCredit.transferFrom(marketplaceOwner, msg.sender, quantity);
+                carbonCredit.updateConsumerBalance(buyer, filled, false);
 
-                // need to transfer contracts from marketplace to this firmId
-                carbonCredit.transferFrom(address(this), msg.sender, filledSoFar);
-                carbonCredit.updateConsumerBalance(firmId, quantity, false);
-
-                if (filledSoFar == 0) {return (filledSoFar, 0);}
-                else {return (filledSoFar, totalPaid / filledSoFar);}
+                if(filled == 0){return (filled, 0);}
+                else {return (filled, totalPaid/filled);}
             }
 
-            uint currPrice = prices[0];
-            uint currQty = creditsForSale[currPrice].qty;
-            uint yetToFill = quantity - filledSoFar;
 
-            if (currQty > yetToFill) {// this is the case where this is the last listing we have to look at
-                filledSoFar += yetToFill;
-                totalPaid += currPrice * yetToFill;
+            uint minIdx = getMinIdx(prices);
+            uint currPrice = prices[minIdx];
+            uint currQty = qty[minIdx];
+            uint yetToFill = quantity - filled;
+
+            if(currQty > yetToFill){
+                filled += yetToFill;
+                totalPaid += yetToFill*currPrice;
                 creditsForSale[currPrice].qty -= yetToFill;
-                // after this we break out of the loop
+                
+                qty[minIdx] -= yetToFill;
             }
-
-            else if (currQty < yetToFill) {// if this listing is still not enough to completely fill our order
-                filledSoFar += currQty;
+            else if(currQty <= yetToFill){
+                filled += currQty;
                 // assign all
                 totalPaid += currPrice * currQty;
 
-                // now we have to pop the array and re-sort
-                delete prices[0];
-                prices[0] = prices[numListings - 1];
-                numListings -= 1;
-                insertionSort(prices, numListings);
+                // now we have to pop the array and delete listing
+                delete prices[minIdx];
+                delete qty[minIdx];
 
+                numListings -= 1;
+                prices[minIdx] = prices[numListings];
+                qty[minIdx] = qty[numListings];
+                
+                
                 delete creditsForSale[currPrice];
             }
+
         }
+        carbonCredit.transferFrom(marketplaceOwner, msg.sender, quantity);
+        carbonCredit.updateConsumerBalance(buyer, filled, false);
 
-        // transfer contracts from marketplace to this firmId
-        carbonCredit.transferFrom(address(this), msg.sender, filledSoFar);
-        carbonCredit.updateConsumerBalance(firmId, quantity, false);
-
-        return (filledSoFar, totalPaid / filledSoFar);
-    }
-
-    function insertionSort(uint[] memory data, uint numElems) internal pure {
-        uint length = numElems;
-        for (uint i = 1; i < length; i++) {
-            uint key = data[i];
-            uint j = i - 1;
-            while ((int(j) >= 0) && (data[j] > key)) {
-                data[j + 1] = data[j];
-                j--;
-            }
-
-            data[j + 1] = key;
-        }
+        return (filled, totalPaid / filled);
     }
 
 }
